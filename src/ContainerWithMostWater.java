593acda7a97e2d85cf5c13a38d71c03fe5fc237a
Ankita <mailtoankita6@gmail.com>
2024-11-15 20:47:45 +0530

    code

--- src/ContainerWithMostWater.java
+++ src/ContainerWithMostWater.java
@@ -16,4 +16,20 @@ public class ContainerWithMostWater {
         }
         return area;
     }
+
+//less running time solution
+//    public int maxArea(int[] height) {
+//        int left=0;
+//        int right=height.length-1;
+//        int max=0;
+//        while(left<right){
+//            int min=Math.min(height[left],height[right]);
+//            max=Math.max(max,min*(right-left));
+//            while(left < right && height[left]<=min)
+//                left++;
+//            while(left < right && height[right]<=min)
+//                right--;
+//        }
+//        return max;
+//    }
 }
