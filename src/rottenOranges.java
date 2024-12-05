40d5a2e30615fa07bb1929f661a342834e9e1f02
Ankita <mailtoankita6@gmail.com>
2024-12-06 00:06:10 +0530

    code

--- src/rottenOranges.java
+++ src/rottenOranges.java
@@ -1,6 +1,5 @@
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
 
 public class rottenOranges {
     public static void main(String[] args) {
@@ -18,54 +17,34 @@ public class rottenOranges {
     }
 
     private static int rotOranges(int[][] arr) {
-        int n = arr.length;
-        int m = arr[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        int[] delRow = {-1,0,1 ,0};
-        int[] delcol = {0,1,0,-1};
-        int [][]vis = new int[n][m];
-        int freshCount =0;
-        for (int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(arr[i][j] == 2)
-                {
-                    q.add(new int[]{i,j,0});
-                    vis[i][j] =2;
-                }
-                else
-                {
-                    vis[i][j]=0;
-                }
-                if(arr[i][j]==1)
-                    freshCount++;
+        int row = arr.length, col = arr[0].length;
+        int freshoranges = 0, time = 0;
+        Deque<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (arr[i][j] == 2)
+                    queue.add(new int[]{i, j});
+                else if (arr[i][j] == 1)
+                    freshoranges++;
             }
         }
-        int tm=0, cnt =0;
-        while (!q.isEmpty())
-        {
-            int curr[] =q.poll();
-            int row =curr[0];
-            int col = curr[1];
-            int t = curr[2];
-
-            tm = Math.max(tm,t);
-
-            for(int i=0;i<4;i++)
-            {
-                int newRow = row + delRow[i];
-                int newCol = col + delcol[i];
-                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && arr[newRow][newCol] == 1 &&  vis[newRow][newCol] != 2 )
-                {
-                    vis[newRow][newCol] = 2;
-                    q.add(new int[]{ newRow,newCol, t+1});
-                    cnt++;
+        int direction[] = {1, 0, -1, 0, 1};
+        while (!queue.isEmpty() && freshoranges > 0) {
+            time++;
+            for (int i = queue.size(); i > 0; i--) {
+                int position[] = queue.poll();
+                for (int j = 0; j < 4; j++) {
+                    int x = position[0] + direction[j];
+                    int y = position[1] + direction[j + 1];
+                    if (x >= 0 && x < row && y >= 0 && y < col && arr[x][y] == 1) {
+                        arr[x][y] = 2;
+                        freshoranges--;
+                        queue.offer(new int[]{x, y});
+                    }
                 }
             }
-
         }
+        return (freshoranges>0?-1:time);
 
-return (cnt == freshCount) ? tm : -1;
     }
 }
